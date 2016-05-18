1297409990e906af4acafc1084da2d38cdf93ed5
Barry Ma <barrymaning@gmail.com>
2016-05-18 15:10:33 -0400

    initial commit

---
+++ SocialAppTemplate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialAppTemplate
+//
+//  Created by Barry Ma on 2016-05-18.
+//  Copyright © 2016 Barry Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
